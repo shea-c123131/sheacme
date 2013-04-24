b6ee3a84281f62dfc07ac1cf3640aa4e749e48db
Shea Conlon v8 <sheaconlon8@macmini.local>
2013-04-23 19:05:28 -0700

    fix?

--- web.js
+++ web.js
@@ -11,6 +11,31 @@ app.get('/', function(request, response) {
 	if(process.env.ACCKEYID){
 		acc_key_id=process.env.ACCKEYID;
 		sec_acc_key=process.env.SECACCKEY;
+		  			dynamodb = new AWS.DynamoDB({
+		accessKeyId:acc_key_id,
+		secretAccessKey:sec_acc_key,
+		region: 'us-east-1'
+	});
+  				dynamodb.putItem({
+		TableName:'nodedb',
+		Item:{
+			path:{S:'something'},
+			time:{N:'1'},
+			categories:{S:'e'},
+			somet:{S:String(Date.now())}
+		}
+	}, function(err, data){
+		
+	});
+		dynamodb.getItem({
+		TableName:'nodedb',
+		Key:{
+			path:{S:'something'},
+			time:{N:'1'}
+		}
+	}, function(err, data){
+		response.send(data['Item']['somet']['S']);
+	});
 	}else{
 		fs.readFile('acc_key_id.txt', 'utf8', function (err,data) {
   			acc_key_id=data;
